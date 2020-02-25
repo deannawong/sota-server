d2fc6295e4e2669d06eb40cf7958e7bc12832388
Deanna Wong <dwong221@gmail.com>
2020-02-24 21:55:17 -0500

    initial attempt to deploy server

---
+++ server/api/activities.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const { Activity } = require("../db");
+
+router.get("/", (req, res, next) => {
+  Activity.findAll()
+    .then(allActivities => {
+      if (allActivities.length) {
+        res.status(200).send(allActivities);
+      } else {
+        res.status(404).send("No activities found!");
+      }
+    })
+    .catch(err => next(err));
+});
+
+router.get("/:id", (req, res, next) => {
+  const { id } = req.params;
+  Activity.findByPk(id).then(foundActivity => {
+    if (foundActivity) {
+      res.status(200).send(foundActivity);
+    } else {
+      res.status(404).send(`No activity found with id ${id}`);
+    }
+  });
+});
+
+router.post("/", (req, res, next) => {
+  Activity.create(req.body).then(newActivity =>
+    res.status(201).send(newActivity)
+  );
+});
+
+module.exports = router;
