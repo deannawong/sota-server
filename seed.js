9dc214aee219b072b97c4ebae9b2bea6dfbdc0c5
Deanna Wong <dwong221@gmail.com>
2020-02-26 16:37:26 -0500

    added single user to seed

---
+++ seed.js
@@ -0,0 +1,35 @@
+const {
+  Activity,
+  ActivityInstance,
+  Itinerary,
+  User,
+  db,
+  sync
+} = require("./server/db");
+const axios = require("axios");
+
+const seed = () => {
+  return sync(true).then(() => {
+    User.create({
+      firstName: "James",
+      lastName: "Doe",
+      city: "New York City",
+      email: "jamesdoe@gmail.com",
+      password: "secret"
+    });
+  });
+};
+module.exports = seed;
+
+if (require.main === module) {
+  seed()
+    .then(() => {
+      console.log(chalk.greenBright("Successful seeding in SOTA server."));
+      db.close();
+    })
+    .catch(err => {
+      console.error(chalk.redBright("Error with seeding SOTA server!"));
+      console.error(err);
+      db.close();
+    });
+}
