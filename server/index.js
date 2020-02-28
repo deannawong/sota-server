d54057d0f398c17bdccd19f2b7b227617b995d98
Jake Hertz <hertzjake@gmail.com>
2020-02-28 10:02:14 -0500

    testing credentials with allow origin localhost:8100

--- server/index.js
+++ server/index.js
@@ -40,14 +40,15 @@ app.use(express.urlencoded({ extended: true }));
 // });
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  // res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET, POST, OPTIONS, PUT, PATCH, DELETE'
   );
-  // //
-  // res.header('Access-Control-Allow-Credentials', 'true');
-  // //
+  //
+  res.header('Access-Control-Allow-Credentials', 'true');
+  //
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
