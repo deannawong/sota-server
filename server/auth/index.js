26a79e3d38fb8148ae2c70954f6aa02ab1449650
Jake Hertz <hertzjake@gmail.com>
2020-02-27 22:14:50 -0500

    installing moment

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const moment = require('moment');
 const { User, Session } = require('../db/index');
 
 router.post('/login', (req, res, next) => {
@@ -10,7 +11,7 @@ router.post('/login', (req, res, next) => {
     .then(userOrNull => {
       if (userOrNull && !userOrNull.isPasswordValid(req.body.password)) {
         console.error('invalid password');
-        res.sendStatus(400);
+        return res.sendStatus(400);
       }
       if (!userOrNull) return res.sendStatus(401);
       // console.log('cookies in log in post: ', req.cookies);
