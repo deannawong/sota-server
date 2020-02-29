950720e76e3303cdc20f4a13c80746f65bceed46
Jake Hertz <hertzjake@gmail.com>
2020-02-29 13:56:28 -0500

    fix check token function

--- server/middleware.js
+++ server/middleware.js
@@ -2,12 +2,13 @@ const jwt = require('jsonwebtoken');
 // JWT
 const checkToken = (res, req, next) => {
   // might need to add these headers to accepted CORS
-  console.log('req in checkToken: ', req);
-  let token = req.headers['x-access-token'] || req.headers['authorization'];
+  console.log('req headers in checkToken: ', req.headers);
+  let token = req.headers['authorization'];
+  console.log('token before slice: *******************************', token);
   if (token.startsWith('Bearer')) {
     token = token.slice(7, token.length);
   }
-  console.log('token after slice in checkToken: ', token);
+  console.log('token after slice in checkToken: *******************', token);
   if (token) {
     jwt.verify(token, procses.env.JWT_TOKEN, (err, decoded) => {
       if (err) {
