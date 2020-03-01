b06a754c7330337ee2f5b9fdad4c30a5540a62e3
Deanna Wong <dwong221@gmail.com>
2020-03-01 13:20:04 -0500

    deleted unnecessary activity model/routes

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 router.use("/users", require("./users"));
 router.use("/itineraries", require("./itineraries"));
 router.use("/activityInstances", require("./activityInstances"));
-router.use("/activities", require("./activities"));
 
 router.use((req, res, next) => {
   const err = new Error("API route not found!");
