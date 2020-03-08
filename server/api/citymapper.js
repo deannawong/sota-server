3e3f2b2f0e96085103cf67be1f910d34b30e439c
Jake Hertz <hertzjake@gmail.com>
2020-03-08 10:44:21 -0400

    trying with many requests back and forth

--- server/api/citymapper.js
+++ server/api/citymapper.js
@@ -33,93 +33,31 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-  try {
-    const {
-      date,
-      startTime,
-      startLocationLat,
-      startLocationLong,
-      endLocationLat,
-      endLocationLong,
-    } = req.body.newItinerary;
-
-    const { scheduledActivities } = req.body;
-    const { locationLat, locationLong } = scheduledActivities[0];
-    const dateToUse = date.split('T')[0];
-    const firstDate = new Date(
-      `${date.split('T')[0]} ${startTime}`
-    ).toISOString();
-    const dateToSend = firstDate.split(':').join('%3');
-
-    const newActs = [];
-    axios
-      .get(
-        `https://developer.citymapper.com/api/1/traveltime/?startcoord=${startLocationLat}%2C${startLocationLong}&endcoord=${locationLat}%2C${locationLong}&time=${dateToSend}&time_type=arrival&key=${process.env.CITY_MAPPER_API}`
-      )
-      .then(res => {
-        const firstTransit = res.data;
-        firstTransit.types = 'transit';
-        newActs.push(firstTransit);
-      })
-      .then(async () => {
-        const actsToSend = scheduledActivities.reduce(
-          async (acc, activity, idx) => {
-            acc.push(activity);
-            const { locationLat, locationLong, endTime } = activity;
-            const firstDate = new Date(`${dateToUse} ${endTime}`).toISOString();
-            const dateToSend = firstDate.split(':').join('%3');
-            let nextLat;
-            let nextLng;
-            if (idx < scheduledActivities.length - 1) {
-              const nextActivity = scheduledActivities[idx + 1];
-              nextLat = nextActivity.locationLat;
-              nextLng = nextActivity.locationLong;
-            } else {
-              nextLat = endLocationLat;
-              nextLng = endLocationLong;
-            }
-            // const transitObj = (
-            //   await axios.get(
-            //     `https://developer.citymapper.com/api/1/traveltime/?startcoord=${locationLat}%2C${locationLong}&endcoord=${nextLat}%2C${nextLng}&time=${dateToSend}&time_type=arrival&key=${process.env.CITY_MAPPER_API}`
-            //   )
-            // ).data;
-
-            axios
-              .get(
-                `https://developer.citymapper.com/api/1/traveltime/?startcoord=${locationLat}%2C${locationLong}&endcoord=${nextLat}%2C${nextLng}&time=${dateToSend}&time_type=arrival&key=${process.env.CITY_MAPPER_API}`
-              )
-              .then(res => {
-                const transitObj = res.data;
-                transitObj.types = 'transit';
-                acc.push(transitObj);
-              });
-
-            // console.log('transit object: ', transitObj);
-
-            // without time
-            // const transitObj = (
-            //   await axios.get(
-            //     `https://developer.citymapper.com/api/1/traveltime/?startcoord=${locationLat}%2C${locationLong}&endcoord=${nextLat}%2C${nextLng}&time_type=arrival&key=${cityMapperAPIKey}`
-            //   )
-            // ).data;
-            // transitObj.types = 'transit';
-            // acc.push(activity);
-            // acc.push(transitObj);
-            return acc;
-          },
-          newActs
-        );
-        console.log('acts to send: ', actsToSend);
-        res.status(200).send(actsToSend);
-      })
-      .catch(e => {
-        console.log('error getting from city mapper');
-        console.error(e);
-      });
-  } catch (e) {
-    console.log('error posting on city mapper route');
-    console.error(e);
-  }
+  const {
+    date,
+    startLocationLat,
+    startLocationLong,
+    endLocationLat,
+    endLocationLong,
+  } = req.body;
+  const dateToUse = date.split('T')[0];
+  const firstDate = new Date(
+    `${date.split('T')[0]} ${startTime}`
+  ).toISOString();
+  const dateToSend = firstDate.split(':').join('%3');
+  axios
+    .get(
+      `https://developer.citymapper.com/api/1/traveltime/?startcoord=${startLocationLat}%2C${startLocationLong}&endcoord=${endLocationLat}%2C${endLocationLong}&time=${dateToSend}&time_type=arrival&key=${process.env.CITY_MAPPER_API}`
+    )
+    .then(response => {
+      const transitObj = response.data;
+      transitObj.types = 'transit';
+      res.status(200).send(transitObj);
+    })
+    .catch(e => {
+      console.log('error with city mapper');
+      console.error(e);
+    });
 });
 
 module.exports = router;
