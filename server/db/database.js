c11b98d5f1426ee65a32d18e25050b9939614ef9
Deanna Wong <dwong221@gmail.com>
2021-05-16 19:24:59 -0400

    update sequelize configs

--- server/db/database.js
+++ server/db/database.js
@@ -3,11 +3,35 @@ const chalk = require('chalk');
 const dbName = 'capstone_travel_app';
 console.log(chalk.yellowBright(`opening database connection to ${dbName}`));
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
-  {
+// const db = new Sequelize(
+//   process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
+//   {
+//     logging: false,
+//   }
+// );
+const DATABASE_URL = `postgres://localhost:5432/${dbName}`
+let config;
+if (process.env.DATABASE_URL) {
+  config = {
     logging: false,
-  }
-);
+    operatorsAliases: false,
+    dialect: 'postgres',
+    protocol: 'postgres',
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  };
+} else {
+  config = {
+    logging: false,
+    operatorsAliases: 0,
+  };
+}
+
+const db = new Sequelize(DATABASE_URL, config);
 
 module.exports = db;
